fa57a3699670ab6f22b860f5863f8d78fcdaac94
Костадин Бундов <kb14713085@edu.mon.bg>
2022-03-09 17:56:45 +0200

    Create migrations

---
+++ Data/Migrations/202203091040019_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Password = c.String(),
+                        FirstName = c.String(),
+                        MiddleName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        PhoneNumber = c.String(),
+                        Birthdate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
